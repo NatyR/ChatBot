0585df805bf7537de547d853c78894cd156199dc
Renata Felix <naty.bmth1@gmail.com>
2019-04-29 02:34:49 -0300

    Desenvolvendo diálogo entre Bot e Usuário, simulando o pedido de uma pizza

--- README.md
+++ README.md
@@ -38,8 +38,14 @@ npm start
 Abrir o .exe do BOT FRAMEWORK EMULATOR, e executar a aplicação com o link do nosso servidor:
 ```
 http://localhost:3978/api/messages
+<<<<<<< HEAD
+```
+
+Porfim, basta interagir com o Bot via chat.
+=======
 
 ```
 
 Porfim, basta interagir com o Bot via chat.
 
+>>>>>>> 7c0b28fa1386c885fa6c26807aaa5bf8b303cb52
--- index.js
+++ index.js
@@ -10,17 +10,64 @@ server.listen(process.env.port || process.env.PORT || 3978, function() {
 
 //Criando um conector com o Bot Framework
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: '',
+    appPassword: ''
 });
 
-//Criando 2 ActionS: POST para receber as mensagens dos usuários e
+//Criando 2 Actions: POST para receber as mensagens dos usuários e
 //e outra action com um request para que o Bot interaja com os usuário
 server.post('/api/messages', connector.listen());
 
-var bot = new builder.UniversalBot(connector, function (session){
+
+//Função inicial apenas para o Bot repetir o conteúdo digitado
+/*var bot = new builder.UniversalBot(connector, function (session){
     session.send("Você disse: %s", session.message.text);
-});
+});*/
+const bot = new builder.UniversalBot(connector);
+
+//Bloco de Diálogos
+bot.dialog("/", [session => {
+    builder.Prompts.text(session, "Olá, como vc se chama?");
+}, (session, results) => {
+    let nome = results.response;
+    session.send(`Olá, ${nome}`);
+    session.beginDialog("/perguntarSaborPizza");
+}]);
+
+bot.dialog("/perguntarSaborPizza", [session => {
+    builder.Prompts.text(session, "Qual o sabor de Pizza que deseja pedir?");
+}, (session, results) => {
+    let saborPizza = results.response;
+    session.endDialog(`Puxa que legal! Então vc gosta de comer pizza de ${saborPizza}! Este é um dos sabores que saem com frequência aqui :)`);
+    session.beginDialog("/perguntarCepCliente");
+}]);
+
+bot.dialog("/perguntarCepCliente", [session => {
+    builder.Prompts.text(session, "Qual o CEP para entrega da melhor pizza que comerá hj?");
+}, (session, results) => {
+    let CEP = results.response;
+    session.endDialog(`Certo, estamos quase finalizando`);
+    session.beginDialog("/perguntarFormaPagamento");
+}]);
+
+bot.dialog("/perguntarFormaPagamento", [session => {
+    builder.Prompts.text(session, "Qual forma de pagamento vc prefere?");
+}, (session, results) => {
+    let formaPgmt = results.response;
+    session.endDialog(`Certo, então a forma de pagamento escolhida será então ${formaPgmt}.`);
+    session.beginDialog("/finalizarAtendimento");
+}]);
+
+bot.dialog("/finalizarAtendimento", [session => {
+    builder.Prompts.text(session, "Seu pedido foi finalizado! Sua deliciosa pizza chegará entre 30-40 min e já estamos confeccionando ela pra vc recebê-la quentinha em sua casa viu?!");
+}, (session, results) => {
+    session.endDialog(`Qualquer dúvida poderá retornar aqui ou ligar para (11)4747-9861. Obrigada!!`);
+}]);
+
+
+
+
+
 
 
 
